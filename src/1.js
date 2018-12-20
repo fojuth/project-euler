efe49a0593e44dd16c0f95e6c281c8d3f4420c6a
Kamil Fojuth <fen@desmart.com>
2018-12-20 23:02:57 +0100

    Init; first problem solved

---
+++ src/1.js
@@ -0,0 +1,25 @@
+const filter = predicate => list => list.filter(predicate)
+const isMultipleOf = num => value => value % num === 0
+const isMultipleOfThreeOrFive = num => isMultipleOf(3)(num) || isMultipleOf(5)(num)
+const rangeFromOne = n => [...Array(n - 1).keys()].map(n => n + 1)
+const sum = list => list.reduce(
+  (acc, val) => acc + val, 0
+)
+
+const pipe = (...fns) => initial => (
+  fns.reduce(
+    (acc, fn) => fn(acc),
+    initial
+  )
+)
+
+const sumNaturalMultiplesOfThreeOrFive = limit => (
+  pipe(
+    rangeFromOne,
+    filter(isMultipleOfThreeOrFive),
+    sum
+  )(limit)
+)
+
+sumNaturalMultiplesOfThreeOrFive(1000)
+
